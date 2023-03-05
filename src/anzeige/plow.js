efe755f212d1ffe727401fb5bfa6373b8b763718
Patrick Richter <gratach@gmx.de>
2023-03-05 12:24:15 +0100

    bugfix_#2

--- src/anzeige/plow.js
+++ src/anzeige/plow.js
@@ -10,6 +10,7 @@ export class Plow{
 	}
 	startPlowing(){
 		this.farthestIn = 0;
+		this.farthestOut = 0;
 		this.closestOut = 100000000;
 		this.totalCount = 0;
 		for(let x of this.herde.allParticles){
@@ -18,9 +19,12 @@ export class Plow{
 					this.farthestIn = x.distance;
 				this.totalCount += 1;
 			}
-			else
+			else{
+				if(x.distance > this.farthestOut)
+					this.farthestOut = x.distance;
 				if(x.distance < this.closestOut)
 					this.closestOut = x.distance
+			}
 		}
 		if(!this.waitForPlowing){
 				this.waitForPlowing = true;
@@ -28,78 +32,54 @@ export class Plow{
 		}
 	}
 	plowLayer(){
-		if(this.farthestIn > this.closestOut || this.farthestIn == -1){
-			/*// find how mutch space is neaded
-			let spaceNeaded = this.totalCount - this.maxParticles;
-			for(let x of this,herde.allParticles){
-				if(!x.solid && x.distance < 
-			}*/
-			for(let x of this.herde.partikels){
-				if(x.solid && (x.distance == this.farthestIn || x.distance == -1)){
-					x.vanish();
-					this.totalCount -= 1;
-				}
+		// remove all particles that are unconnected to focus
+		for(let x of this.herde.partikels){
+			if(x.solid && x.distance == -1){
+				x.vanish();
+				this.totalCount -= 1;
 			}
-			this.farthestIn -= 1;
-			let layerFull = true;
+		}
+		
+		//remove particles from most outer layer if necessary
+		if(this.farthestIn > this.closestOut || this.maxParticles < this.totalCount){
+			// find how mutch space is neaded
+			let spaceNeaded = this.totalCount - this.maxParticles;
 			for(let x of this.herde.allParticles){
-				if(this.totalCount >= this.maxParticles){
-					layerFull = false;
+				if(!x.solid && x.distance < this.farthestIn)
+					spaceNeaded += 1;
+			}
+			console.log("spaceNeaded", spaceNeaded)
+			// iterate to remove particles
+			for(let x of this.herde.partikels){
+				if(spaceNeaded <= 0){
+					this.farthestIn += 1;
 					break;
 				}
-				if(!x.solid && x.distance == this.closestOut){
-					x.appear()
-					this.totalCount += 1;
-				}
-			}
-			if(layerFull){
-				this.closestOut += 1;
-				if(this.totalCount < this.maxParticles && this.farthestIn < this.closestOut){
-					console.log("refill")
-					for(let x of this.herde.allParticles){
-						if(!x.solid && x.distance == this.closestOut){
-							x.appear()
-							this.totalCount += 1;
-						}
-						if(this.totalCount >= this.maxParticles){
-							this.waitForPlowing = false;
-							return;
-						}
-					}
-					this.closestOut += 1;
-				}
-			}
-			setTimeout(()=>{this.plowLayer()}, 500);
-		}
-		else if(this.totalCount > this.maxParticles){
-			for(let x of this.herde.partikels){
-				if(x.solid && (x.distance == this.farthestIn || x.distance == -1)){
+				if(x.solid && (x.distance == this.farthestIn)){
 					x.vanish();
 					this.totalCount -= 1;
-				}
-				if(this.totalCount <= this.maxParticles){
-					this.waitForPlowing = false;
-					return
+					spaceNeaded -= 1;
 				}
 			}
 			this.farthestIn -= 1;
-			setTimeout(()=>{this.plowLayer()}, 500);
 		}
-		else if(this.totalCount < this.maxParticles){
-			console.log("refill")
-			for(let x of this.herde.allParticles){
-				if(!x.solid && x.distance == this.closestOut){
-					x.appear()
-					this.totalCount += 1;
-				}
-				if(this.totalCount >= this.maxParticles){
-					this.waitForPlowing = false;
-					return;
-				}
+		
+		//add particles to lowest layer if necessary
+		for(let x of this.herde.allParticles){
+			if(this.totalCount >= this.maxParticles){
+				this.closestOut -= 1;
+				break;
 			}
-			this.closestOut += 1;
-			setTimeout(()=>{this.plowLayer()}, 500);
+			if(!x.solid && x.distance == this.closestOut){
+				x.appear()
+				this.totalCount += 1;
+			}
 		}
+		this.closestOut += 1;
+		
+		//repeat if necessary;
+		if(this.closestOut < this.farthestIn || this.maxParticles < this.totalCount || (this.maxParticles > this.totalCount && this.closestOut <= this.farthestOut))
+			setTimeout(()=>{this.plowLayer()}, 500);
 		else
 			this.waitForPlowing = false;
 	}
